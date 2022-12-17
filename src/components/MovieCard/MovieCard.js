9184c74827ef974fe088e026a5013af19cece680
serhatbek <serhat.bekk@gmail.com>
2022-12-17 22:31:28 +0300

    movie card structure

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ghostImage = 'https://picsum.photos/seed/picsum/300/444';
 
 const MovieCard = () => {
-  return <div className='movie-card'>MovieCard</div>;
+  return (
+    <div className='movie-card'>
+      <figure>
+        <img src={ghostImage} alt='' />
+      </figure>
+      <div className='movie-card__info'>
+        <h4>Title</h4>
+        <h5>Year</h5>
+        <Link to={`/`} className='link-btn'>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
