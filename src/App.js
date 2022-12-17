c9d23af94a7b9de8451c0ff8b8c043a268f55c13
serhatbek <serhat.bekk@gmail.com>
2022-12-17 22:17:20 +0300

    setting router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './scss/styles.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout/Layout';
+import Home from './pages/Home/Home';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 function App() {
-  return <main className='App'>App</main>;
+  return (
+    <main>
+      <Routes>
+        <Route path='redux-moviemania' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='movie/:movieId' element={<MovieDetail />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
